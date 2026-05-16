4241f8ce4618cb029f88c97be4baa59a71ff612b
TGvenomYT <rajicbe1969@gmail.com>
2026-05-16 11:07:17 +0530

    Update HallOfFame.js

--- components/HallOfFame.js
+++ components/HallOfFame.js
@@ -152,7 +152,7 @@ export default function HallOfFame() {
                           alt={topper.name}
                           fill
                           sizes="(max-width: 768px) 100vw, 50vw"
-                          className="object-cover object-[center_20%] transition-transform duration-700 group-hover:scale-105"
+                          className={`object-cover transition-transform duration-700 group-hover:scale-105 ${topper.featured ? "object-top" : "object-[center_20%]"}`}
                           unoptimized
                         />
                       )}
